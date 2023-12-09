aab17ddbea30d61c11ffe644cf1f7085780d25bb
Moiz Ali <127224240+HoodClock@users.noreply.github.com>
2023-12-09 08:16:18 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function ShowSideBar() {
+    const sidebarMenu = document.getElementById('sidebar');
+    sidebarMenu.style.display = "flex";
+}
+function HideSideMenu() {
+    const sidebarMenu = document.getElementById('sidebar');
+    sidebarMenu.style.display = "none"
+}
